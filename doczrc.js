fcf1a047f0419d7f51a894c1c8506a25b480851b
Nick <nfzaremba@gmail.com>
2021-05-04 22:58:00 -0700

    updated styling

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,20 @@
+const colors = Object.freeze({
+  dark: {
+    background: "#12181b",
+    text: "#fff",
+    paragraph: "#b2becd", // used for paragraphs, ul, ol, etc.
+    accent: "#9166cc", // used for inlinecode text color
+    muted: "#1d1d1d", // used for inlinecode background color
+    blue: "#1fb6ff",
+  },
+  light: {
+    text: "#000000",
+    paragraph: "#828282",
+    accent: "#c7254e",
+    muted: "#f9f2f4",
+  },
+});
+
 export default {
   title: "Zaremba Docz",
   menu: ["Components", "JavaScript", "Getting Started", "Readme"],
@@ -9,26 +26,24 @@ export default {
     colors: {
       modes: {
         dark: {
-          background: "#12181b",
-          text: "#fff",
-          paragraph: "#b2becd", // used for paragraphs, ul, ol, etc.
-          accent: "#9166cc", // used for inlinecode text color
-          muted: "#1d1d1d", // used for inlinecode background color
-          blue: "#1fb6ff",
-          Container: {
-            bg: "bg",
-          },
-          root: {
-            bg: "bg",
-          },
-          // sidebar: {
-          //   // boxShadow: "4px 0 10px -3px #010101",
-          //   bg: "#13161F",
-          //   navGroup: "text",
-          //   navLinkActive: "blue",
-          //   tocLink: "#1fb6ff",
-          //   tocLinkActive: "yellow",
+          //...colors.dark,
+          // Container: {
+          //   bg: "bg",
           // },
+          // root: {
+          //   bg: "bg",
+          // },
+          header: {
+            bg: "#13161F",
+          },
+          sidebar: {
+            // boxShadow: "4px 0 10px -3px #010101",
+            bg: "#13161F",
+            navGroup: "text",
+            navLinkActive: colors.dark.blue,
+            tocLink: "#1fb6ff",
+            tocLinkActive: "#1cffea",
+          },
           prism: {
             plain: {
               bg: "red",
@@ -36,28 +51,17 @@ export default {
           },
         },
         light: {
-          text: "#000000",
-          paragraph: "#828282",
-          accent: "#c7254e",
-          muted: "#f9f2f4",
+          ...colors.light,
         },
       },
     },
     styles: {
-      Container: {
-        bg: "background",
-      },
-      root: {
-        bg: "background",
-      },
-      sidebar: {
-        // boxShadow: "4px 0 10px -3px #010101",
-        bg: "#13161F",
-        navGroup: "text",
-        navLinkActive: "blue",
-        tocLink: "#1fb6ff",
-        tocLinkActive: "yellow",
-      },
+      // Container: {
+      //   bg: "background",
+      // },
+      // root: {
+      //   bg: "background",
+      // },
       code: {
         bg: "yellow",
       },
