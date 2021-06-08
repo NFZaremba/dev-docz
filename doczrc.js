1410ae71f2d16548136837246f740123bc2ce7c2
Nick <nfzaremba@gmail.com>
2021-06-07 22:19:50 -0700

    updates

--- doczrc.js
+++ doczrc.js
@@ -1,15 +1,13 @@
 const colors = {
   dark: {
     background: "#12181b", // used for main container bg
-    text: "#fff",
-    paragraph: "#b2becd", // used for paragraphs, ul, ol, etc.
+    text: "#000",
     accent: "#1fb6ff", // used for inlinecode text color
     muted: "#2d2d2d", // used for inlinecode background color
   },
   light: {
     background: "#f7f6f3",
     text: "#000000",
-    paragraph: "#828282",
     accent: "#c7254e",
     muted: "#f9f2f4",
     gray: "#67788a",
@@ -17,10 +15,11 @@ const colors = {
   common: {
     gray: "#67788a",
     white: "#fff",
-    shadow: "#00000040",
+    shadow: "#3b4048",
     black: "#000",
     green: "#00ff08",
     pink: "#ff00d4",
+    purple: "#5f00f8",
   },
 };
 
@@ -37,7 +36,7 @@ export default {
         dark: {
           ...colors.dark,
           sidebar: {
-            navGroup: "text",
+            navGroup: colors.dark.white,
             navLinkActive: colors.dark.accent,
             tocLink: colors.dark.accent,
             tocLinkActive: colors.dark.accent,
@@ -55,42 +54,42 @@ export default {
       },
     },
     styles: {
-      Container: {
-        "> h2": {
-          position: "relative",
-          fontSize: "1.5em",
-          fontWeight: "bold",
-          padding: "10px 0px 10px 70px",
-          margin: "30px -26px 20px -26px",
-          color: colors.common.white,
-          background: "linear-gradient( 90deg,rgb(0 0 0) ,rgb(98 0 255))",
-          textShadow: `0px 1px 2px ${colors.common.black}`,
-          boxShadow: "0px 2px 4px #3b4048",
-          ":after": {
-            content: '" "',
-            position: "absolute",
-            width: 0,
-            height: 0,
-            left: 0,
-            top: "100%",
-            borderWidth: "5px 10px",
-            borderStyle: "solid",
-            borderColor: "#666 #666 transparent transparent",
-          },
-          ":before": {
-            content: '" "',
-            position: "absolute",
-            width: 0,
-            height: 0,
-            left: "calc(100% - 20px);",
-            top: "100%",
-            borderWidth: "5px 10px",
-            borderStyle: "solid",
-            borderColor: "#666 #666 transparent transparent",
-            transform: "scale(-1, 1)",
-          },
-        },
-      },
+      // Container: {
+      //   "> h1": {
+      //     position: "relative",
+      //     fontSize: "1.5em",
+      //     fontWeight: "bold",
+      //     padding: "10px 0px 10px 70px",
+      //     margin: "30px -26px 20px -26px",
+      //     color: colors.common.white,
+      //     background: "linear-gradient( 90deg,rgb(0 0 0) ,rgb(98 0 255))",
+      //     textShadow: `0px 1px 2px ${colors.common.black}`,
+      //     boxShadow: "0px 2px 4px #3b4048",
+      //     ":after": {
+      //       content: '" "',
+      //       position: "absolute",
+      //       width: 0,
+      //       height: 0,
+      //       left: 0,
+      //       top: "100%",
+      //       borderWidth: "5px 10px",
+      //       borderStyle: "solid",
+      //       borderColor: "#666 #666 transparent transparent",
+      //     },
+      //     ":before": {
+      //       content: '" "',
+      //       position: "absolute",
+      //       width: 0,
+      //       height: 0,
+      //       left: "calc(100% - 20px);",
+      //       top: "100%",
+      //       borderWidth: "5px 10px",
+      //       borderStyle: "solid",
+      //       borderColor: "#666 #666 transparent transparent",
+      //       transform: "scale(-1, 1)",
+      //     },
+      //   },
+      // },
       blockquote: {
         marginLeft: "15px",
         marginLeft: "15px",
@@ -99,27 +98,17 @@ export default {
         paddingLeft: "25px",
         paddingRight: "25px",
         bg: "none",
-        borderLeft: `1px solid ${colors.common.black}`,
-        borderTop: `1px solid ${colors.common.black}`,
-        boxShadow: `5px 10px 8px ${colors.common.shadow}`,
-        background:
-          "linear-gradient( 90deg,rgb(216 0 255 / 50%) ,rgba(0 173 255 / 50%))",
+        boxShadow: `6px 5px 10px ${colors.common.shadow}`,
+        background: "#fff",
+        border: "none",
         "> h1": {
-          color: "text",
-          textShadow: `3px 2px 2px rgb(98 0 255)`,
-          padding: "20px 28px",
-          borderTop: "5px solid #00ff08",
-          margin: "-1px -25px 20px -25px",
-        },
-        "> h2": {
           position: "relative",
-          fontSize: "1.5em",
+          fontSize: "2.5em",
           fontWeight: "bold",
-          padding: "10px 0px 10px 25px",
+          padding: "20px 0px 20px 25px",
           margin: "-1px -45px 20px -45px",
           color: colors.common.white,
           background: "linear-gradient( 90deg,rgb(0 0 0) ,rgb(98 0 255))",
-          textShadow: `0px 1px 2px ${colors.common.black}`,
           boxShadow: "0px 2px 4px #3b4048",
           ":after": {
             content: '" "',
@@ -145,18 +134,40 @@ export default {
             transform: "scale(-1, 1)",
           },
         },
-        "> p": {
-          mb: "16px",
+        " > h2": {
+          marginTop: "3rem",
+          "> a": {
+            color: colors.common.white,
+            position: "relative",
+            background: colors.common.black,
+            padding: "10px 20px",
+            marginLeft: "-45px",
+            boxShadow: `2px 2px 4px ${colors.common.purple}`,
+            borderTopRightRadius: "30px",
+            borderBottomRightRadius: "30px",
+            color: colors.common.white,
+            ":after": {
+              content: '" "',
+              position: "absolute",
+              width: 0,
+              height: 0,
+              left: 0,
+              top: "100%",
+              borderWidth: "5px 10px",
+              borderStyle: "solid",
+              borderColor: "#666 #666 transparent transparent",
+            },
+          },
         },
         "> h3": {
           "> a": {
             position: "relative",
-            background: "linear-gradient( 90deg,rgb(0 0 0) ,rgb(98 0 255))",
-            padding: "10px 20px",
+            background: colors.common.black,
+            padding: "5px 20px",
             marginLeft: "-35px",
-            boxShadow: "0px 2px 4px #3b4048",
-            borderTopRightRadius: "30px",
+            boxShadow: `0px 2px 4px ${colors.common.shadow}`,
             borderBottomRightRadius: "30px",
+            color: colors.common.white,
             ":after": {
               content: '" "',
               position: "absolute",
@@ -172,6 +183,7 @@ export default {
         },
         "> h4": {
           "> a": {
+            color: colors.common.black,
             // position: "relative",
             // background: "linear-gradient( 90deg,rgb(0 0 0) ,rgb(98 0 255))",
             // padding: "10px 20px",
@@ -181,31 +193,33 @@ export default {
             // borderBottomRightRadius: "30px",
           },
         },
+        "> p": {
+          mb: "10px",
+        },
         "> table": {
           color: "#fff",
         },
       },
       inlineCode: {
         bg: "muted",
-        color: colors.common.pink,
-        padding: "2px 4px",
+        color: colors.common.green,
+        padding: "2px 6px",
         borderRadius: "4px",
-        fontWeight: 700,
         fontSize: ".85em",
       },
       a: {
-        color: colors.common.green,
+        color: colors.common.purple,
       },
       strong: {
+        fontStyle: "italic",
         color: "text",
-        fontWeight: 700,
-        // boxShadow: `inset 0 -1px 0 ${colors.common.gray}`,
+        fontWeight: 900,
+        boxShadow: `inset 0 -4px 0 ${colors.common.green}`,
       },
       em: {
-        color: colors.common.green,
         fontStyle: "italic",
-        fontWeight: 700,
-        textShadow: `0px 1px 2px ${colors.common.black}`,
+        color: colors.common.purple,
+        fontWeight: 900,
       },
       img: {
         display: "block",
@@ -217,19 +231,21 @@ export default {
         marginBottom: "2.5rem",
       },
       p: {
-        color: "paragraph",
-        textShadow: `0px 1px 2px ${colors.common.black}`,
+        fontStyle: "normal",
+        color: "text",
+        fontWeight: 500,
       },
       ol: {
-        color: "paragraph",
+        color: "text",
         paddingLeft: "25px",
-        textShadow: `0px 1px 2px ${colors.common.black}`,
+        fontWeight: 600,
+        marginBottom: "10px",
       },
       ul: {
-        color: "paragraph",
+        color: "text",
         paddingLeft: "25px",
         listStyleType: "square",
-        textShadow: `0px 1px 2px ${colors.common.black}`,
+        fontWeight: 600,
       },
       hr: {
         borderBottom: `1px solid ${colors.common.gray}`,
